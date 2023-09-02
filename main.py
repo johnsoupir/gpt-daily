9be3333c45e7ea6377efb913fa666da38cd3e7c5
John Soupir <johnsoupir@gmail.com>
2023-09-01 22:00:18 -0500

    Adds fetchNews and generateSummary functions

--- main.py
+++ main.py
@@ -17,16 +17,21 @@ def load_KEY(keyfile):
         print("An error occurred opening the API key file: ", e)
 
 
-
 def fetchNews(entryIndex):
     NewsFeed = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/US.xml")
-    entry = NewsFeed.entries[0]
-    return entry
+    entry = NewsFeed.entries[entryIndex]
+    return (entry.title + " " + entry.summary)
+
+def generateSummary(input, length):
+    summary = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo-16k',
+        messages=[{"role":"user", "content": "In " + str(length) + " words summarize: " + news}]
+    )
+    return summary
+
 
 # tts_model = TTS.list_models(Self: TTS)[0]
-
 # tts = TTS(tts_model)
-
 # wav = tts.tts("This is a test! This is also a test!!", speaker=tts.speakers[0], language=tts.languages[0])
 
 
@@ -35,19 +40,24 @@ openai.api_key = load_KEY("API_KEY")
 # availableModels = openai.Model.list()
 
 
-
 # print("The following AI models are available via the API:filename")
 # print(availableModels)
 
 news = fetchNews(1)
-print("\n\nNews: ", news.summary)
+print("\n\nNews: ", news)
+
+# print("\n\nNews Title: ", news.title)
+# print("\n\nNews: ", news.summary)
+
 output = openai.ChatCompletion.create(
     model='gpt-3.5-turbo-16k',
-    messages=[{"role":"user", "content": "In 5 words summarize: " + news.summary  }]
+    messages=[{"role":"user", "content": "In 5 words summarize: " + news}]
 )
+
 print("\n\nAI Summary: ")
 print(output.choices[0].message.content)
 
+print(generateSummary(fetchNews(0),8))
 
 
 #Get API key from file
