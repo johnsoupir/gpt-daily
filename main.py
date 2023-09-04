f8839ff1c78715954c27b3c3277b794834430c5e
John Soupir <johnsoupir@gmail.com>
2023-09-04 14:49:28 -0500

    Adds google TTS, RSS feeds, functions for news

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
-#import openAI API
 from typing import Self
 import openai
 import feedparser
 from TTS.api import TTS
+from pydub import AudioSegment
+from pydub.playback import play
+from datetime import datetime
+from google.cloud import texttospeech
+
+nytRSS = "https://rss.nytimes.com/services/xml/rss/nyt/US.xml"
+hackadayRSS = "https://hackaday.com/blog/feed/"
+arsTechnicaRSS = "https://feeds.arstechnica.com/arstechnica/index"
+
+def googleTTS(inputText):
+    client = texttospeech.TextToSpeechClient()
+    synthInput = texttospeech.SynthesisInput(text=inputText)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", 
+        name="en-US-Neural2-I",
+        ssml_gender=texttospeech.SsmlVoiceGender.MALE
+
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        input = synthInput, voice=voice, audio_config=audio_config
+    )
+
+    with open("podcast.mp3", "wb") as out:
+        out.write(response.audio_content)
+
+
+def getStringDate():
+    current_date = datetime.now()
+    day = current_date.day
+    month = current_date.strftime('%B')
+    year = current_date.strftime('%Y')
+    # Determine the suffix for the day
+    if 4 <= day <= 20 or 24 <= day <= 30:
+        suffix = "th"
+    else:
+        suffix = ["st", "nd", "rd"][day % 10 - 1]
+    # Construct the date string
+    date_string = f"{month} {day}{suffix} {year}"
+    return date_string
 
 def load_KEY(keyfile):
     try:
@@ -16,51 +59,84 @@ def load_KEY(keyfile):
     except Exception as e:
         print("An error occurred opening the API key file: ", e)
 
-
-def fetchNews(entryIndex):
-    NewsFeed = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/US.xml")
+def fetchNews(feed, entryIndex):
+    NewsFeed = feedparser.parse(feed)
     entry = NewsFeed.entries[entryIndex]
-    return (entry.title + " " + entry.summary)
+    return (entry.title + ". " + entry.summary + ". " + entry.content[0].value)
 
 def generateSummary(input, length):
     summary = openai.ChatCompletion.create(
         model='gpt-3.5-turbo-16k',
-        messages=[{"role":"user", "content": "In " + str(length) + " words summarize: " + news}]
+        messages=[{"role":"user", "content": "In " + str(length) + " words summarize: " + input}]
     )
-    return summary
+    return summary.choices[0].message.content + " "
+
+def editPodcast(podcast):
+    edit = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo-16k',
+        messages=[{"role":"user", "content": "Improve this podcast script, add transitions between stories and outro, do not alter facts:\n " + podcast }]
+    )
+    return edit.choices[0].message.content + " "
 
 
-# tts_model = TTS.list_models(Self: TTS)[0]
-# tts = TTS(tts_model)
-# wav = tts.tts("This is a test! This is also a test!!", speaker=tts.speakers[0], language=tts.languages[0])
+# Init TTS
+model_name = 'tts_models/en/ljspeech/tacotron2-DDC'
+tts = TTS(model_name)
 
-
+# Init OpenAI
 openai.api_key = load_KEY("API_KEY")
-
+# print("The following AI models are available via the API:")
 # availableModels = openai.Model.list()
-
-
-# print("The following AI models are available via the API:filename")
 # print(availableModels)
 
-news = fetchNews(1)
-print("\n\nNews: ", news)
 
-# print("\n\nNews Title: ", news.title)
-# print("\n\nNews: ", news.summary)
-
-output = openai.ChatCompletion.create(
-    model='gpt-3.5-turbo-16k',
-    messages=[{"role":"user", "content": "In 5 words summarize: " + news}]
-)
-
-print("\n\nAI Summary: ")
-print(output.choices[0].message.content)
-
-print(generateSummary(fetchNews(0),8))
+# Get News
 
 
-#Get API key from file
+podcastIntro = "Welcome to Gee Pee Tee Daily. Your Artificial Inteligence powered flash briefing on today's top stories. Let's dive in."
+podcast = podcastIntro
+podcast += ("Today is " + getStringDate() + ". ")
+podcast += "From the new york times, "
 
-#Get response from API
+for story in range(1):
+
+    news = fetchNews(nytRSS,story)
+    print("\n\nNews: ", news)
+    podcast += generateSummary(news,20)
+
+podcast += "Now, news from hackaday: "
+
+for hackaday in range(1):
+    news = fetchNews(hackadayRSS, hackaday)
+    podcast += generateSummary(news, 30)
+
+podcast += "From Ars Technica: "
+
+for ars in range(1):
+    news = fetchNews(arsTechnicaRSS, ars)
+    podcast += generateSummary(news, 30)
+
+# print("\n\nAI Summary: ")
+# print(podcast, "\n\n")
+
+
+
+# tts.tts_to_file(text=podcast, file_path="output.wav")
+
+# Load the WAV file
+# sound = AudioSegment.from_file("output.wav")
+
+# Play the sound
+# play(sound)
+
+print("______ EDITED PODCAST _________")
+editedPodcast = editPodcast(podcast)
+print(editedPodcast)
+# tts.tts_to_file(text=editedPodcast, file_path="output.wav")
+# sound = AudioSegment.from_file("output.wav")
+# play(sound)
+
+googleTTS(editedPodcast)
+sound = AudioSegment.from_file("podcast.mp3")
+play(sound)
 
